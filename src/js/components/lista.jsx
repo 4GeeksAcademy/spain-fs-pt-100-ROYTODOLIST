0f507ebef99e0354cc773336eeb7cdc7841a9e9c
hedrickantonio <rhantoni.cuba@gmail.com>
2025-04-07 22:11:32 +0000

    principio

--- src/js/components/lista.jsx
+++ src/js/components/lista.jsx
@@ -3,21 +3,39 @@ import React, { useState,useEffect } from "react";
 
 
 //create your first component
-const Listado = () => {
+const Listado = (props) => {
 	
 const [data,setData] = useState([]);
 const [tarea,setTarea] = useState('');
 
+const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHovering(true);
+    
+  };
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
+
 const handleSubmit = e =>{
-    e.preventDefault();
+    e.preventDefault();//para que la pagina no se recargue
     setData([...data,{tarea}]);
     setTarea('');
 }
 const handleChangeTarea = (e)=>{
 	setTarea(e.target.value)
-	
+
+ 
 }
-    
+
+const handleElim =(index) =>{
+console.log(index)
+let aux = data.filter((el,i)=> i!=index)
+setData(aux)
+}
+
     return (
 
  
@@ -28,9 +46,16 @@ const handleChangeTarea = (e)=>{
 <div className="col-auto">
     <button type="submit"  hidden className="btn btn-primary mb-3"></button>
   </div>
-<ul className="list-group">
-  {data.map((el,i)=><li class="list-group-item" key={i}>{el.tarea}</li> )}
-
+<ul className="list-group"> 
+  {/* llaves en un return significa que voy a ejecutar javascript */}
+  
+  {data.map((el,i)=><div className="row">
+    <div className="col-sm-12 " >
+      <li  onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="list-group-item d-flex justify-content-between" key={i}>{el.tarea}<div   onClick={()=>handleElim(i)} >
+    {isHovering && (
+        <div className="barra__escritura"></div> 
+        )}</div></li> </div></div>)}
+  
 </ul>
 </form>
 </div>
