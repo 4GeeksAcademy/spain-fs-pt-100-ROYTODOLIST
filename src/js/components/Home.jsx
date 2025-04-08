6226e22dd70f3bbc3d18eb3a288ce09c3735185a
hedrickantonio <rhantoni.cuba@gmail.com>
2025-04-08 16:33:29 +0000

    listo

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -5,7 +5,25 @@ import Listado from "./lista";
 //create your first component
 const Home = () => {
 	return (
-		<Listado />
+		<div className="image">
+
+			<div className="row">
+
+				<span className="Letra">Todo List </span>
+
+			</div>
+			<div className="row d-flex flex-column align-items-center">
+				<div className="col-sm-12 col-lg-6">
+				<div class="card bg-transparent"><Listado /></div>
+				</div>
+
+
+
+			</div>
+
+		</div>
+
+
 	);
 };
 
--- src/js/components/lista.jsx
+++ src/js/components/lista.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState } from "react";
 
 
 
@@ -7,6 +7,7 @@ const Listado = (props) => {
 	
 const [data,setData] = useState([]);
 const [tarea,setTarea] = useState('');
+
 
 const [isHovering, setIsHovering] = useState(false);
 
@@ -23,6 +24,7 @@ const handleSubmit = e =>{
     e.preventDefault();//para que la pagina no se recargue
     setData([...data,{tarea}]);
     setTarea('');
+ 
 }
 const handleChangeTarea = (e)=>{
 	setTarea(e.target.value)
@@ -30,8 +32,16 @@ const handleChangeTarea = (e)=>{
  
 }
 
+const totales =()=>{
+return data.length
+
+}
+let total=totales()
+
+console.log (totales());
+
 const handleElim =(index) =>{
-console.log(index)
+
 let aux = data.filter((el,i)=> i!=index)
 setData(aux)
 }
@@ -42,24 +52,30 @@ setData(aux)
  <div className="row d-flex flex-column m-5 align-items-center">
     <div className="col-lg-8 " >
  <form onSubmit={handleSubmit}>       
-<input  value={tarea} onChange={handleChangeTarea} type="text" className="form-control" id="exampleFormControlInput1" placeholder="Whats needs to be done?" />
-<div className="col-auto">
+<input  value={tarea} onChange={handleChangeTarea} type="text" className="form-control bg-transparent" id="exampleFormControlInput1" placeholder="Whats needs to be done?" />
+<div className="col">
     <button type="submit"  hidden className="btn btn-primary mb-3"></button>
   </div>
-<ul className="list-group"> 
+<ul className="list-group list-group-flush"> 
   {/* llaves en un return significa que voy a ejecutar javascript */}
   
   {data.map((el,i)=><div className="row">
     <div className="col-sm-12 " >
-      <li  onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="list-group-item d-flex justify-content-between" key={i}>{el.tarea}<div   onClick={()=>handleElim(i)} >
+      <li  onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className="list-group-item d-flex justify-content-between bg-transparent" key={i}>{el.tarea}<div   onClick={()=>handleElim(i)} >
     {isHovering && (
         <div className="barra__escritura"></div> 
         )}</div></li> </div></div>)}
   
 </ul>
+<div className="card-footer d-flex align-items-center "><p className="Letra1 m-1">Items</p>
+  {total}
+</div>
 </form>
 </div>
+
 </div>
+
+
 	);
 };
 
